4da5b0d2505f7cdef1586ab58622511fc4add098
Coder-King3 <2196592083@qq.com>
2025-06-16 03:10:05 +0800

    fix: 适配部署vercel

--- routes/[...].ts
+++ routes/[...].ts
@@ -1,25 +1,9 @@
-import { readFileSync } from 'fs'
-import { resolve } from 'path'
-
 export default defineEventHandler((event) => {
-  // 设置响应头
-  event.node.res.setHeader('Content-Type', 'text/html; charset=utf-8')
-
-  try {
-    // 获取HTML文件的绝对路径
-    const filePath = resolve(process.cwd(), '.output', 'public', 'welcome.html')
-    console.log(`🚀 filePath:`, filePath)
-
-    // 读取HTML文件内容
-    const htmlContent = readFileSync(filePath, 'utf-8')
-
-    // 设置响应头并返回HTML内容
-    return htmlContent
-  } catch (error) {
-    console.error('读取HTML文件失败:', error)
-    return `
-      <h1>错误</h1>
-      <p>无法加载欢迎页面</p>
-    `
+  // console.log(`🚀 event:`, event)
+  // 获取当前请求路径
+  const path = event.path || getRequestURL(event).pathname
+  // 如果当前路径不是 /welcome.html 和 /favicon.ico ，才进行重定向
+  if (path !== '/welcome.html' && path !== '/favicon.ico') {
+    return sendRedirect(event, '/welcome.html')
   }
 })
