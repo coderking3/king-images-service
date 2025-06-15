7495c993c8b8531a36a0a2c89c4a12a9eed70297
Coder-King3 <2196592083@qq.com>
2025-06-15 23:21:59 +0800

    fix: 修复多次重定向问题

--- routes/[...].ts
+++ routes/[...].ts
@@ -1,3 +1,9 @@
 export default defineEventHandler((event) => {
-  return sendRedirect(event, '/welcome.html')
+  // console.log(`🚀 event:`, event)
+  // 获取当前请求路径
+  const path = event.path || getRequestURL(event).pathname
+  // 如果当前路径不是 /welcome.html 和 /favicon.ico ，才进行重定向
+  if (path !== '/welcome.html' && path !== '/favicon.ico') {
+    return sendRedirect(event, '/welcome.html')
+  }
 })
